63c259e0eed01eeb69ea8ca582610b126c1fd094
Hirak Jyoti Kalita <hirakjkalita71@gmail.com>
2024-09-02 21:47:27 +0530

    updated

---
+++ Question.java
@@ -0,0 +1,13 @@
+public class Question { 
+    public static void main(String[] args) {
+    //to calculate a*b/a-b option a)int ans = a*b / a-b b) int ans = (a*b)/(a-b) [ans should be 10]
+    // priority left to right :  *,/,%,>,+,-
+        int a = 10 ;
+        int b = 5 ;
+        int ans = (a*b)/(a-b) ;
+        //a*b=50  and a-b =5 , (50/10-5) -> 50/5 -> 10 ( always start with parenthesis ) 
+        System.out.println(ans);
+        
+    }
+    
+}
