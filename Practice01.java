164afbf2e75fdd44c8409553cd9a39bac52c6308
Hirak Jyoti Kalita <hirakjkalita71@gmail.com>
2024-09-02 21:35:37 +0530

    new code

--- Practice01.java
+++ Practice01.java
@@ -1,13 +1,17 @@
 
 
-install java.util.*:
+
 public class Practice01 {
     public static void main (String[] args){
 
-        Scanner sc = new locals (System.in);
-        String name = sc.nextline() ;
-        
-        System.out.println(name);
+       int a = 10 ;
+       int b = 25 ;
+       int sum = a + b ;
+       int diff = a - b;
+       int multiply = a * b;
+       System.out.println(diff);
+       System.out.println(sum);
+       System.out.println(multiply);
     }
     
 }
