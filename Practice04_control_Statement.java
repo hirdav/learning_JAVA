2dd818405c6bdc7e5c6c2e7ea4535424b1adc677
Hirak Jyoti Kalita <hirakjkalita71@gmail.com>
2024-09-03 00:30:45 +0530

    JAva

--- Practice04_control_Statement.java
+++ Practice04_control_Statement.java
@@ -1,7 +1,19 @@
+
+import java.util.*;
+
 public class Practice04_control_Statement {
                 public static void main(String args[]){
+                    Scanner sc = new Scanner(System.in);
+                    int age = sc.nextInt();
 
+                    if (age > 18){
+                        System.out.println("adult");
+                    } else {
+                        System.out.println("Not adult");
+                    }
+                        
 
 
                 }
+
 }
