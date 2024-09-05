d02a56a51896191de0e2a5ac80a6647ce7d85e9c
Hirak Jyoti Kalita <hirakjkalita71@gmail.com>
2024-09-05 12:03:57 +0530

    one question done

--- Practice_Question_5Loops.java
+++ Practice_Question_5Loops.java
@@ -2,21 +2,35 @@ import java.util.*;
 
 public class Practice_Question_5Loops {
     
-    //print the sum of n natural number
+    //Q1:print the sum of n natural number
     public static void main (String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int sum = 0 ;
-        for(int i=0 ; i<=n; i++ )
-            {
-                sum = sum + i ;
-            }
+        // System.out.println("enter the natural number:  ");
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int sum = 0 ;
+        // for(int i=0 ; i<=n; i++ )
+        //     {
+        //         sum = sum + i ;
+        //     }
 
-        System.out.println(sum);
+        // System.out.println(sum);
         
+        //Q2 Print the table of a number input by the user 
+        System.out.println("Which table you want to learn? ");
+        Scanner sc= new Scanner(System.in);
+        int table_no= sc.nextInt();
+        System.out.println("So here are your table for: "+table_no); 
+        int i =0;
+        int result = 0;
+        
+        for(i=1;i<=10; i++){
+            result = table_no * i;
+            System.out.println( table_no + "x" +i + "=" +result);
 
+        }
+
+        }
 
     }
 
     
-}
