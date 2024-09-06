1fbed58fb04f6e1a987c4008e1cf4cb0ce237717
Hirak Jyoti Kalita <hirakjkalita71@gmail.com>
2024-09-07 01:35:15 +0530

    holo  rectanlge doen

--- Star_Design.java
+++ Star_Design.java
@@ -1,8 +1,9 @@
 public class Star_Design {
     public static void main(String[] args) {
-        System.out.println("making a star rectangle ");
+        
         
        // Q1 : print the rectangle-star pattern 
+       //System.out.println("making a star rectangle ");
     //    int n = 4; // Number of rows
     //     int m = 5; // Number of stars per row
         
@@ -16,18 +17,25 @@ public class Star_Design {
     //         System.out.println();
                  
         //Q2 : print the start patern 
-
+        System.out.println("making a holo star rectangle ");
         
-        int m = 5;
         int n = 4;
+        int m = 5;
         
 
         //outer loop
-        for (int i=1;i<=n;i++){
+        for (int i=1; i<=n; i++){
+            //inner loop
             for(int j=1 ; j<=m; j++) {
-                    
 
-                System.out.print(" * ");
+                //cell ->(i,j)
+                 if (i == 1 || j == 1 || i == n || j == m){
+                    System.out.print(" * ");
+                 }  else  {
+                        System.out.print(" 0 ");
+                 }
+
+                
             }
         
               System.out.println();
