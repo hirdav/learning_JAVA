4ec519a8965e89d5d75d76e30f008be626520c25
Hirak Jyoti Kalita <hirakjkalita71@gmail.com>
2024-09-08 10:34:29 +0530

    boolean statement testing

--- Practice_Question_1.java
+++ Practice_Question_1.java
@@ -8,6 +8,12 @@ public class Practice_Question_1 {
         //a*b=50  and a-b =5 , (50/10-5) -> 50/5 -> 10 ( always start with parenthesis ) 
         System.out.println(ans);
         
+        // to test the true or false of a statement and it cannt be chain statement like x<y<z
+        int x = 5;
+        int y = 10;
+        int z = 15;
+        System.out.println("ans : " + (x < y && y < z));
+        System.out.println("ans : " + (x < y || y < z));
     }
     
 }
--- Star_Design.java
+++ Star_Design.java
@@ -2,6 +2,7 @@ public class Star_Design {
     public static void main(String[] args) {
         
         
+        
        // Q1 : print the rectangle-star pattern 
        //System.out.println("making a star rectangle ");
     //    int n = 4; // Number of rows
@@ -17,34 +18,42 @@ public class Star_Design {
     //         System.out.println();
                  
         //Q2 : print the start patern 
-        System.out.println("making a holo star rectangle ");
+        // System.out.println("making a holo star rectangle ");
         
-        int n = 4;
-        int m = 5;
+        // int n = 4;
+        // int m = 5;
         
 
-        //outer loop
-        for (int i=1; i<=n; i++){
-            //inner loop
-            for(int j=1 ; j<=m; j++) {
+        // //outer loop
+        // for (int i=1; i<=n; i++){
+        //     //inner loop
+        //     for(int j=1 ; j<=m; j++) {
 
-                //cell ->(i,j)
-                 if (i == 1 || j == 1 || i == n || j == m){
-                    System.out.print(" * ");
-                 }  else  {
-                        System.out.print(" 0 ");
-                 }
+        //         //cell ->(i,j)
+        //          if (i == 1 || j == 1 || i == n || j == m){
+        //             System.out.print(" * ");
+        //          }  else  {
+        //                 System.out.print(" 0 ");
+        //          }
 
                 
-            }
+        //     }
         
-              System.out.println();
+        //       System.out.println();
 
-
-
+        //Q : Half pyramid
+              int n = 4;
+        
+        //outer loop
+        for (int i=1; i<=n; i++) {
+            //inner loop
+            for(int j=1 ; j<=i; j++) {
+                    System.out.print("*");        
+                 }   
+                 System.out.println();
+            }
+                
         }
-
-
     }
-}
+
 
