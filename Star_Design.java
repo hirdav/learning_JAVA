6e2464b4a1f3f4dadf6b0b3431d0c22b166ab17b
Hirak Jyoti Kalita <hirakjkalita71@gmail.com>
2024-09-11 23:01:30 +0530

    updates

--- Star_Design.java
+++ Star_Design.java
@@ -140,24 +140,36 @@ public class Star_Design {
         //                 0 1 0 1
         //                 1 0 1 0 1
 
+            // int n =5;
+
+            // for (int i=1 ; i <=n ; i++){
+            //     for (int j=1 ; j<=i ; j++){
+            //         int sum = i+j;
+            //         if (sum %2 ==0) //even 
+            //         {
+            //             System.out.print("1");
+            //         } else //odd
+            //         {
+            //             System.out.print("0");
+            //         }
+            //     }System.out.println();
+            // }
+
             int n =5;
 
-            for (int i=1 ; i <=n ; i++){
-                for (int j=1 ; j<=i ; j++){
-                    int sum = i+j;
-                    if (sum %2 ==0) //even 
-                    {
-                        System.out.print("1");
-                    } else //odd
-                    {
-                        System.out.print("0");
+            for (int i=n ; i >=1 ; i--){
+                for (int j=1 ; j<=n-1 ; j++){
+                    
+                        System.out.println("*");
+                    
+                }
                     }
-                }System.out.println();
+                System.out.println();
             }
         }
 
         
-    }
+    
  
     
 
